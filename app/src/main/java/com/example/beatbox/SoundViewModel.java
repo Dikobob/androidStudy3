1167304cba2e2e258f1b487889273ff04ee107a4
DidenkoBV <Didenko.B.V@sberbank.ru>
2019-06-03 14:15:51 +0300

    stage 20 is DONE!

---
+++ app/src/main/java/com/example/beatbox/SoundViewModel.java
@@ -0,0 +1,27 @@
+package com.example.beatbox;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+public class SoundViewModel extends BaseObservable {
+    private Sound mSound;
+    private BeatBox mBeatBox;
+
+    public SoundViewModel(BeatBox beatBox) {
+        mBeatBox = beatBox;
+    }
+
+    public Sound getSound() {
+        return mSound;
+    }
+
+    @Bindable
+    public String getTitle() {
+        return mSound.getName();
+    }
+
+    public void setSound(Sound mSound) {
+        this.mSound = mSound;
+        notifyChange();
+    }
+}
